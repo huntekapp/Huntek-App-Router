8fde1c0a089474a77563ae9a4b1853c296fe4d45
Matias Gigena <tute910@gmail.com>
2023-07-03 19:55:24 -0300

    repo

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
-export default function Home() {
+import Image from "next/image";
+
+export default function Landing() {
   return (
-    <div>
-      <h1>Hello HUNTEK!</h1>
+    <div className="h-screen grid place-content-center">
+      <Image src="/huntek.svg" width={240} height={266} quality={100} />
     </div>
   );
 }
