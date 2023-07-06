ee46e12f7493413383eda21cd67af6321810fd6f
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-07-06 20:56:17 -0300

    App

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,14 @@
 "use client";
 import {useRouter} from "next/navigation";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 
 export default function Landing() {
 	const router = useRouter();
-	const [activatePing, setActivatePing] = useState(false);
 
 	const animate = () => {
 		const timeout = setTimeout(() => {
-			setActivatePing(true);
-			setTimeout(() => {
-				router.push("/login");
-			}, 200);
-		}, 4600);
+			router.push("/login");
+		}, 4000);
 		return () => clearTimeout(timeout);
 	};
 
@@ -22,7 +18,7 @@ export default function Landing() {
 
 	return (
 		<section className="h-screen bg-pri grid place-content-center">
-			<article className={`w-64 h-64 bg-HKanimate ${activatePing && "animate-ping duration-200"}`}></article>
+			<article className="w-64 h-64 bg-HKanimate"></article>
 		</section>
 	);
 }
