db90f88dfcf7714f5629db241b70262f8b042d1e
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-07-06 04:59:34 -0300

    SignUp Style

--- app/components/signup.jsx
+++ app/components/signup.jsx
@@ -1,5 +1,5 @@
 "use client";
-import Image from "next/image";
+import Link from "next/link";
 import {useState} from "react";
 
 const SignForm = () => {
@@ -30,83 +30,76 @@ const SignForm = () => {
 	};
 
 	return (
-		<main className="w-full h-screen p-4 flex flex-col items-center justify-center sm:px-4">
-			<div className="w-full space-y-6 text-sec sm:max-w-md">
-				<div className="p-4 py-6 space-y-8 sm:p-6 sm:rounded-lg">
-					<a href="/login">
-						<Image src="/utils/back.svg" width={20} height={20} alt="back" />
-					</a>
-					<h1 className="text-4xl">Sign Up</h1>
-					<form onSubmit={handleSubmit} className="space-y-5">
-						<div>
-							<label className="font-medium">First Name</label>
-							<input
-								type="text"
-								placeholder="Your First Name"
-								value={input.firstName}
-								name="firstName"
-								onChange={handleChange}
-								className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent border-b"
-							/>
-						</div>
-						<div>
-							<label className="font-medium">Last Name</label>
-							<input
-								type="text"
-								placeholder="Your Last Name"
-								value={input.lastName}
-								name="lastName"
-								onChange={handleChange}
-								className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border-b"
-							/>
-						</div>
-						<div>
-							<label className="font-medium">E-mail</label>
-							<input
-								type="email"
-								placeholder="✉️ Your Email"
-								value={input.email}
-								name="email"
-								onChange={handleChange}
-								className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border-b "
-							/>
-						</div>
-						<div>
-							<label className="font-medium">Password</label>
-							<input
-								type="password"
-								placeholder="🔒 Your Password"
-								value={input.password}
-								name="password"
-								onChange={handleChange}
-								className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border-b"
-							/>
-						</div>
-						<div>
-							<p className="text-gray-400">
-								By signing up you agree to our{" "}
-								<a href="/terms_conditins" className="text-sec hover:underline">
-									Terms & Condition and Privacy Policy.
-								</a>
-							</p>
-						</div>
-						<button className="w-full px-4 py-2 text-pri font-medium bg-sec hover:bg-gray-300 active:bg-lig rounded-lg duration-150">
-							Continue
-						</button>
-					</form>
-				</div>
-				<div className="text-center">
-					<div className="mt-5 space-y-2">
-						<p>
-							Already signed up?{" "}
-							<a href="/login" className="font-medium text-sec hover:underline">
-								Log In
-							</a>
-						</p>
-					</div>
-				</div>
-			</div>
-		</main>
+		<section className="w-11/12 max-w-md h-screen font-medium text-sec flex flex-col items-center justify-evenly">
+			<article className="flex flex-col justify-center items-center">
+				<div className="w-40 h-40 bg-HKlogo bg-cover"></div>
+				<p className="text-3xl">Sign Up</p>
+			</article>
+			<form onSubmit={handleSubmit} className="w-full h-[500px] flex flex-col justify-evenly">
+				<label htmlFor="firstName">
+					First Name
+					<input
+						type="text"
+						name="firstName"
+						value={input.firstName}
+						className="w-full px-3 py-2 bg-transparent outline-none border-b"
+						placeholder="Your First Name"
+						onChange={handleChange}
+					/>
+				</label>
+				<label htmlFor="lastName">
+					Last Name
+					<input
+						type="text"
+						name="lastName"
+						value={input.lastName}
+						className="w-full px-3 py-2 bg-transparent outline-none border-b"
+						placeholder="Your Last Name"
+						onChange={handleChange}
+					/>
+				</label>
+				<label htmlFor="email">
+					E-mail
+					<input
+						type="email"
+						name="email"
+						value={input.email}
+						className="w-full px-3 py-2 bg-transparent outline-none border-b"
+						placeholder="Your Email ✉️"
+						onChange={handleChange}
+					/>
+				</label>
+				<label htmlFor="password">
+					Password
+					<input
+						type="password"
+						name="password"
+						value={input.password}
+						className="w-full px-3 py-2 bg-transparent outline-none border-b"
+						placeholder="Your Password 🔒"
+						onChange={handleChange}
+					/>
+				</label>
+				<p className="text-gray-400">
+					By signing up you agree to our{" "}
+					<Link href="/ter_cond" className="text-sec hover:underline">
+						Terms & Condition and Privacy Policy.
+					</Link>
+				</p>
+				<button className="w-full py-2 text-pri bg-sec hover:bg-gray-300 active:bg-lig rounded-lg duration-150">
+					Continue
+				</button>
+			</form>
+			<article>
+				<p>
+					Already signed up?{" "}
+					<Link href="/login" className="hover:underline">
+						Log In
+					</Link>
+				</p>
+			</article>
+		</section>
 	);
 };
+
 export default SignForm;
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -2,9 +2,9 @@ import SignForm from "../components/signup";
 
 const Signup = () => {
 	return (
-		<div className="h-screen bg-pri flex flex-col justify-evenly items-center">
+		<section className="min-h-screen bg-pri flex flex-col justify-evenly items-center">
 			<SignForm />
-		</div>
+		</section>
 	);
 };
 
