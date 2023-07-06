148a9061208a60f0df66f7957d23c306d7058598
Matias Gigena <tute910@gmail.com>
2023-07-06 01:42:47 -0300

    Versión estable.

--- app/components/footbar.jsx
+++ app/components/footbar.jsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 const Footbar = () => {
   return (
-    <footer className="p-7 mt-3 bg-pri shadow-[0px_-10px_12px_0px_#00000024] relative">
+    <footer className="p-5 mt-3 bg-pri shadow-[0px_-10px_12px_0px_#00000024] fixed bottom-0 w-full">
       <div className="w-full border-3 ">
         <div className="flex justify-center ">
-          <button className="border-2  bg-pri flex justify-center items-center absolute top-0 mt-[-28px] rounded-full w-16 h-16 hover:scale-105 shadow-xl#00000024">
-            <Image src="/huntek/logo/W.svg" width={48} height={10} className="h-18" />
+          <button className="border-2  bg-pri flex justify-center items-center absolute top-0 mt-[-28px] rounded-full w-14 h-14 hover:scale-105 shadow-xl#00000024">
+            <Image src="/huntek/logo/W.svg" width={44} height={10} className="h-auto" />
           </button>
         </div>
       </div>
