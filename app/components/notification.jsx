149f6f958bc8d2efe59b3c0e55d4346509593866
franvazquezz <franvazquez2001@gmail.com>
2023-07-06 12:51:08 -0300

    notifications finished

--- app/components/notification.jsx
+++ app/components/notification.jsx
@@ -1,43 +1,74 @@
+"use client";
 import Image from "next/image";
 import NavBar from "./navbar";
+import {useEffect, useState} from "react";
 
 const Notification = () => {
+	const [notis, setNotis] = useState([
+		{
+			name: "NotifiLorem",
+			value:
+				"Esta es una notificación. Notifica acciones. Nicofront easteregg parte dos. Me parece que quedo lagra lanoti. No acciona notas, no.",
+		},
+		{
+			name: "Notificacion",
+			value:
+				"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio debitis numquam asperiores dolore ea, vitae",
+		},
+		{
+			name: "Notife",
+			value: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis beatae atque qui. Expedita recusandae
+	perferendis cum nihil quos`,
+		},
+		{
+			name: "Notifiuno",
+			value: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis beatae atque qui. Expedita recusandae
+	perferendis cum nihil quos, odio nemo illo eos earum voluptatem fugiat, enim, repudiandae amet ad
+	suscipit.`,
+		},
+		{
+			name: "Notifid",
+			value: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis beatae atque qui. Expedita recusandae
+	perferendis cum nihil quos, odio nemo illo eos earum voluptatem fugiat, enim, repudiandae amet ad
+	suscipit.`,
+		},
+	]);
+
+	const handleClick = (nombre) => {
+		let filteredNotis = notis.filter((noti) => noti.name !== nombre);
+		setNotis([...filteredNotis]);
+	};
+
 	return (
-		<div className=" bg-sec text-pri">
-			<div className=" bg-sec border border-black w-full min-h-screen">
-				<div className="flex justify-start mt-8 gap-4 h-full w-[100vw] items-start flex-col flex-wrap ">
+		<div className=" bg-sec text-pri  w-full ">
+			<div className=" bg-sec bg-opacity-5 min-h-screen">
+				<div className="flex justify-start mt-8 gap-4 h-full w-full items-start flex-col flex-wrap ">
 					<h2 className="ml-10 text-4xl font-bold mb-6 ">Notificaciones</h2>
 					<div className="p-6 flex-grow">
-						<h2 className="text-xl font-bold">Notificación</h2>
-						<p className="mb-4">
-							Esta es una notificación. Notifica acciones. Nicofront easteregg parte dos. Me parece que quedo lagra la
-							noti. No acciona notificaciones, no.
-						</p>
-						<h2 className="text-xl font-bold">Continuación</h2>
-						<p className="mb-4">
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio debitis numquam asperiores dolore ea, vitae
-							minus, distinctio obcaecati reprehenderit eaque voluptatibus deleniti impedit provident possimus, vel quia
-							suscipit! Officia, commodi.
-						</p>
-						<h2 className="text-xl font-bold">Continuación de la continuación</h2>
-						<p className="mb-4">
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis beatae atque qui. Expedita recusandae
-							perferendis cum nihil quos, odio nemo illo eos earum voluptatem fugiat, enim, repudiandae amet ad
-							suscipit.
-            </p>
-            <h2 className="text-xl font-bold">Continuación de la continuación de la continuación</h2>
-						<p className="mb-4">
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis beatae atque qui. Expedita recusandae
-							perferendis cum nihil quos, odio nemo illo eos earum voluptatem fugiat, enim, repudiandae amet ad
-							suscipit.
-            </p>
-            <h2 className="text-xl font-bold">Continuación de la continuación de la continuación de la continuación</h2>
-						<p className="mb-4">
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis beatae atque qui. Expedita recusandae
-							perferendis cum nihil quos, odio nemo illo eos earum voluptatem fugiat, enim, repudiandae amet ad
-							suscipit.
-            </p>
-            <Image width={100} height={100} src="/images/egg.jpg" />
+						{!notis.length && <h2 className="bg-pri p-2 bg-opacity-5 text-xl flex justify-between  items-center font-bold">No hay notificaciones...</h2>}
+						{notis.map((e, index) => {
+							  return (
+								<div key={index}>
+									<div className="shadow hover:shadow-pri rounded-md">
+										<h2 className="bg-pri p-2 bg-opacity-5 text-xl flex justify-between  items-center font-bold">
+											{e.name && `${e.name}`}
+											<button onClick={() => handleClick(e.name)} className="btn z-50 btn-ghost btn-circle ">
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													className="h-6 z-50 w-6"
+													fill="none"
+													viewBox="0 0 24 24"
+													stroke="#046255">
+													<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+												</svg>
+											</button>
+										</h2>
+										<p className="bg-pri px-4 pb-2 bg-opacity-5">{e.value && e.value}</p>
+									</div>
+									<p className=" text-sec bg-opacity-5">a</p>
+								</div>
+							);
+						})}
 					</div>
 				</div>
 			</div>
