5386f218fbd1c956326fa0c2610edf778435711b
nicolasfont1 <nicolasfont15@gmail.com>
2023-07-07 12:47:13 -0300

    feature(navbar)profile picture/backarrow according path

--- app/components/profileExtend.jsx
+++ app/components/profileExtend.jsx
@@ -1,7 +1,7 @@
 "use client";
 import {useState} from "react";
 import Image from "next/image";
-import Link from "next/link";
+import NavBar from "./navbar";
 
 const ProfileExt = () => {
 	const [userData, setUserData] = useState({
@@ -19,35 +19,7 @@ const ProfileExt = () => {
 
 	return (
 		<div className="min-w-screen text-pri min-h-screen">
-			<div className="navbar shadow-xl bg-pri">
-				<Link href="/home">
-					<button className="btn btn-ghost z-0 btn-circle absolute left-2">
-						<div className="indicator">
-							<Image src="/utils/back.svg" alt="backArrow" width={25} height={25} />
-						</div>
-					</button>
-				</Link>
-				<div className="navbar-center absolute left-2/4 translate-x-[-50%]">
-					<h1 className="font-medium text-sec z-50 normal-case text-3xl">Huntek</h1>
-				</div>
-				<button className="btn btn-ghost z-0 btn-circle absolute right-2">
-					<div className="indicator">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-6 w-6"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="#ffffff">
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-							/>
-						</svg>
-					</div>
-				</button>
-			</div>
+			<NavBar/>
 			<h2 className=" text-xl font-bold text-center text-hk mt-4 mb-2">Mi perfil</h2>
 			<div className="flex flex-col justify-center items-center h-22">
 				<div className="w-20 h-20 avatar">
--- app/interviews/page.jsx
+++ app/interviews/page.jsx
@@ -1,9 +1,8 @@
-import Link from "next/link";
-import Image from "next/image";
 import InterviewDetail from "../components/interviewDetail";
 import AppleLogo from "@/public/images/apple-g44d4327db_1920.png";
 import EcoLogo from "@/public/images/eco-ged3b25e46_1920.png";
 import LinkedInLogo from "@/public/images/linkedin-gf141b6fe5_1920.png";
+import NavBar from "../components/navbar";
 
 // Hardcoded Info / enterpiseLogo must be square image
 const interviewInfo = [
@@ -36,35 +35,7 @@ const interviewInfo = [
 const InterviewsPage = () => {
 	return (
 		<main>
-			<div className="navbar shadow-xl bg-pri">
-				<Link href="/home">
-					<button className="btn btn-ghost z-0 btn-circle absolute left-2">
-						<div className="indicator">
-							<Image src="/utils/back.svg" alt="backArrow" width={25} height={25} />
-						</div>
-					</button>
-				</Link>
-				<div className="navbar-center absolute left-2/4 translate-x-[-50%]">
-					<h1 className="font-medium text-sec z-50 normal-case text-3xl">Huntek</h1>
-				</div>
-				<button className="btn btn-ghost z-0 btn-circle absolute right-2">
-					<div className="indicator">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-6 w-6"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="#ffffff">
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-							/>
-						</svg>
-					</div>
-				</button>
-			</div>
+			<NavBar/>
 			<section className="min-h-screen bg-sec flex flex-col text-left">
 				<h1 className="font-semibold text-2xl mt-4 ml-6">Entrevistas confirmadas</h1>
 				{interviewInfo.map((interview) => {
