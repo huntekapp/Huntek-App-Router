79d81be1d7a49e560059d22b9a73efc6e91b1e87
nicolasfont1 <nicolasfont15@gmail.com>
2023-07-07 11:32:35 -0300

    added key property to article / removed from image

--- app/components/postulations.jsx
+++ app/components/postulations.jsx
@@ -4,49 +4,55 @@ import Link from "next/link";
 import {useState} from "react";
 
 const Postulations = () => {
-  const [postulaciones, setPostulaciones] = useState([
+	const [postulaciones, setPostulaciones] = useState([
+		{
+			id: "Huntek",
+			image: "/huntek/logo/G.svg",
+		},
+		{
+			id: "Youtube",
+			image: "/images/youtube-g6a795f8e6_1920.png",
+		},
+		{
+			id: "Google",
+			image: "/images/google-ga4b7784ab_1920.png",
+		},
+		{
+			id: "Bitcoin",
+			image: "/images/bitcoin-gfe80587d1_1920.png",
+		},
+		{
+			id: "Linkedin",
+			image: "/images/linkedin-gf141b6fe5_1920.png",
+		},
+		{
+			id: "Windows",
+			image: "/images/windows-gc6d7a955d_1920.png",
+		},
     {
-      id: "Huntek",
-      image: "/huntek/logo/G.svg",
+      id: "apple",
+      image: "/images/apple-g44d4327db_1920.png"
     },
-    {id: "Youtube", image: "/images/youtube-g6a795f8e6_1920.png"},
-    {
-      id: "Google",
-      image: "/images/google-ga4b7784ab_1920.png",
-    },
-    {
-      id: "Bitcoin",
-      image: "/images/bitcoin-gfe80587d1_1920.png",
-    },
-    {
-      id: "Linkedin",
-      image: "/images/linkedin-gf141b6fe5_1920.png",
-    },
-    {
-      id: "Windows",
-      image: "/images/windows-gc6d7a955d_1920.png",
-    },
-    {id: "apple", image: "/images/apple-g44d4327db_1920.png"},
-  ]);
-  return (
-    <main className="w-full h-40 my-6 flex flex-col justify-around">
-      <div className="flex flex-row justify-between">
-        <p>Mis postulaciones</p>
-        <Link href="" className="font-medium">
-          Ver todas
-        </Link>
-      </div>
-      <section className="carousel carousel-center  w-full p-2  space-x-6 rounded-box">
-        {postulaciones.map((post) => (
-          <article className="carousel-item avatar">
-            <div className="w-16 rounded-full ring ring-pri ring-offset-base-100 ring-offset-2">
-              <Image key={post.id} src={post.image} width={60} height={100} quality={100} />
-            </div>
-          </article>
-        ))}
-      </section>
-    </main>
-  );
+	]);
+	return (
+		<main className="w-full h-40 my-6 flex flex-col justify-around">
+			<div className="flex flex-row justify-between">
+				<p>Mis postulaciones</p>
+				<Link href="" className="font-medium">
+					Ver todas
+				</Link>
+			</div>
+			<section className="carousel carousel-center  w-full p-2  space-x-6 rounded-box">
+				{postulaciones.map((post) => (
+					<article key={post.id} className="carousel-item avatar">
+						<div className="w-16 rounded-full ring ring-pri ring-offset-base-100 ring-offset-2">
+							<Image src={post.image} width={60} height={100} quality={100} />
+						</div>
+					</article>
+				))}
+			</section>
+		</main>
+	);
 };
 
 export default Postulations;
