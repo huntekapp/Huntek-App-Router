c19e4a070835761b9bcc34d4c83d34405cca464c
nicolasfont1 <nicolasfont15@gmail.com>
2023-07-10 11:13:03 -0300

    added conversation button at navbar according to path

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 import Image from "next/image";
 import Profile from "./profile";
 import {useEffect, useState} from "react";
+import ForumIcon from "@mui/icons-material/Forum";
 
 const NavBar = () => {
 	const [close, setClose] = useState(false);
 	const [currentPath, setCurrentPath] = useState("");
 	const backArrowPaths = ["interviews", "profileExtend", "swipe"]; // Add paths that can't access to Profile view.
+	const conversationButtonPaths = ["swipe"];
 	const handleClick = () => {
 		setClose(!close);
 	};
@@ -74,23 +76,34 @@ const NavBar = () => {
 						</svg>
 					</button>
 				) : (
-					<button className="btn btn-ghost z-0 btn-circle">
-						<a href="/notification" className="indicator">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-6 w-6"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="#ffffff">
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-								/>
-							</svg>
-						</a>
-					</button>
+					<>
+						{conversationButtonPaths.includes(currentPath) ? (
+							<button className="btn btn-ghost z-0 btn-circle">
+								<a href="/conversations">
+									<ForumIcon style={{fontSize: "24px", color: "white"}} />
+								</a>
+							</button>
+						) : (
+							""
+						)}
+						<button className="btn btn-ghost z-0 btn-circle">
+							<a href="/notification" className="indicator">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-6 w-6"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="#ffffff">
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+									/>
+								</svg>
+							</a>
+						</button>
+					</>
 				)}
 			</div>
 		</div>
