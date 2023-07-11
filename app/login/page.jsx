45554e3c324357ffb17f6167b0158706f502893e
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-07-11 18:29:25 -0300

    Changes Login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,7 +3,7 @@ import LogForm from "../components/login";
 const Login = () => {
 	return (
 		<section className="h-screen bg-pri flex flex-col lg:flex-row justify-evenly items-center">
-			<article className="w-1/2 h-1/4 lg:w-[450px] lg:h-[450px] bg-HKtext bg-contain bg-center bg-no-repeat"></article>
+			<article className="w-1/2 h-2/5 lg:w-[450px] lg:h-[450px] bg-HKtext bg-contain bg-center bg-no-repeat"></article>
 			<LogForm />
 		</section>
 	);
