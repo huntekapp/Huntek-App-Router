687735c97bb2da056e9f85385addc92dcbb73379
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-07-11 00:26:52 -0300

    Signup

--- app/components/signup.jsx
+++ app/components/signup.jsx
@@ -7,112 +7,109 @@ import {setEmail} from "../globalstore/features/emailSlice";
 import {useDispatch} from "react-redux";
 
 const SignForm = () => {
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const [postUsers, {isLoading}] = usePostUsersMutation();
-  const [input, setInput] = useState({
-    first_name: "",
-    last_name: "",
-    email: "",
-    password: "",
-  });
+	const dispatch = useDispatch();
+	const router = useRouter();
+	const [postUsers, {isLoading}] = usePostUsersMutation();
+	const [input, setInput] = useState({
+		first_name: "",
+		last_name: "",
+		email: "",
+		password: "",
+	});
 
-  const handleChange = (event) => {
-    event.preventDefault();
-    setInput({
-      ...input,
-      [event.target.name]: event.target.value,
-    });
-  };
+	const handleChange = (event) => {
+		event.preventDefault();
+		setInput({
+			...input,
+			[event.target.name]: event.target.value,
+		});
+	};
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      await postUsers(input).unwrap();
-      dispatch(setEmail(input.email));
-    } catch (error) {
-      console.log(error);
-    }
-    setInput({
-      first_name: "",
-      last_name: "",
-      email: "",
-      password: "",
-    });
-    router.push("/verifymail");
-  };
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			await postUsers(input).unwrap();
+			dispatch(setEmail(input.email));
+		} catch (error) {
+			console.log(error);
+		}
+		setInput({
+			first_name: "",
+			last_name: "",
+			email: "",
+			password: "",
+		});
+		router.push("/verifymail");
+	};
 
-  return (
-    <section className="w-11/12 max-w-md h-screen font-medium text-sec flex flex-col items-center justify-evenly">
-      <article className="flex flex-col justify-center items-center">
-        <div className="w-40 h-40 bg-HKlogo bg-cover"></div>
-        <p className="text-3xl">Sign Up</p>
-      </article>
-      <form onSubmit={handleSubmit} className="w-full h-[500px] flex flex-col justify-evenly">
-        <label htmlFor="firstName">
-          First Name
-          <input
-            type="text"
-            name="first_name"
-            value={input.first_name}
-            className="w-full px-3 py-2 bg-transparent outline-none border-b"
-            placeholder="Your First Name"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="lastName">
-          Last Name
-          <input
-            type="text"
-            name="last_name"
-            value={input.last_name}
-            className="w-full px-3 py-2 bg-transparent outline-none border-b"
-            placeholder="Your Last Name"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="email">
-          E-mail
-          <input
-            type="email"
-            name="email"
-            value={input.email}
-            className="w-full px-3 py-2 bg-transparent outline-none border-b"
-            placeholder="Your Email ✉️"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="password">
-          Password
-          <input
-            type="password"
-            name="password"
-            value={input.password}
-            className="w-full px-3 py-2 bg-transparent outline-none border-b"
-            placeholder="Your Password 🔒"
-            onChange={handleChange}
-          />
-        </label>
-        <p className="text-gray-400">
-          By signing up you agree to our{" "}
-          <Link href="/ter_cond" className="text-sec hover:underline">
-            Terms & Condition and Privacy Policy.
-          </Link>
-        </p>
-        <button className="w-full py-2 text-pri bg-sec hover:bg-gray-300 active:bg-lig rounded-lg duration-150">
-          Continue
-        </button>
-      </form>
-      <article>
-        <p>
-          Already signed up?{" "}
-          <Link href="/login" className="hover:underline">
-            Log In
-          </Link>
-        </p>
-      </article>
-    </section>
-  );
+	console.log(input);
+	return (
+		<section className="w-11/12 max-w-md h-3/4 lg:h-2/5 font-medium text-sec flex flex-col justify-evenly items-center">
+			<form onSubmit={handleSubmit} className="w-full h-3/4 lg:h-full flex flex-col justify-evenly">
+				<label htmlFor="firstName">
+					First Name
+					<input
+						type="text"
+						name="first_name"
+						value={input.first_name}
+						className="w-full px-3 pb-2 bg-transparent outline-none border-b"
+						placeholder="Your First Name"
+						onChange={handleChange}
+					/>
+				</label>
+				<label htmlFor="lastName">
+					Last Name
+					<input
+						type="text"
+						name="last_name"
+						value={input.last_name}
+						className="w-full px-3 pb-2 bg-transparent outline-none border-b"
+						placeholder="Your Last Name"
+						onChange={handleChange}
+					/>
+				</label>
+				<label htmlFor="email">
+					E-mail
+					<input
+						type="email"
+						name="email"
+						value={input.email}
+						className="w-full px-3 pb-2 bg-transparent outline-none border-b"
+						placeholder="Your Email"
+						onChange={handleChange}
+					/>
+				</label>
+				<label htmlFor="password">
+					Password
+					<input
+						type="password"
+						name="password"
+						value={input.password}
+						className="w-full px-3 pb-2 bg-transparent outline-none border-b"
+						placeholder="Your Password"
+						onChange={handleChange}
+					/>
+				</label>
+				<p className="text-sm text-gray-400">
+					By signing up you agree to our{" "}
+					<Link href="/ter_cond" className="text-sec hover:underline">
+						Terms & Condition and Privacy Policy.
+					</Link>
+				</p>
+				<button className="w-full py-2 text-pri bg-sec hover:bg-gray-300 active:bg-lig rounded-lg duration-150">
+					Continue
+				</button>
+			</form>
+			<article>
+				<p>
+					Already signed up?{" "}
+					<Link href="/login" className="hover:underline">
+						Log In
+					</Link>
+				</p>
+			</article>
+		</section>
+	);
 };
 
 export default SignForm;
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -2,7 +2,11 @@ import SignForm from "../components/signup";
 
 const Signup = () => {
 	return (
-		<section className="min-h-screen bg-pri flex flex-col justify-evenly items-center">
+		<section className="h-screen text-sec bg-pri flex flex-col lg:flex-row justify-evenly items-center">
+			<article className="w-1/2 h-1/4 lg:w-[500px] lg:h-[500px] flex flex-col justify-center items-center">
+				<div className="w-11/12 h-2/3 bg-HKlogo bg-contain bg-center bg-no-repeat"></div>
+				<p className="text-3xl lg:text-5xl">Sign Up</p>
+			</article>
 			<SignForm />
 		</section>
 	);
