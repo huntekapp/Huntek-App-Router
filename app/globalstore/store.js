b0cfe765228b90917c4aefd71d3e999eefcf84f5
Matias Gigena <tute910@gmail.com>
2023-07-11 16:01:34 -0300

    recoverPwAction finalizado

--- app/globalstore/store.js
+++ app/globalstore/store.js
@@ -4,6 +4,7 @@ import {registerDB} from "./services/useRegister";
 import {setupListeners} from "@reduxjs/toolkit/dist/query";
 import {verifDB} from "./services/useVerifCode";
 import {loginDB} from "./services/useLogin";
+import {recoverDB} from "./services/useRecoverPw";
 
 export const store = configureStore({
   reducer: {
@@ -11,9 +12,15 @@ export const store = configureStore({
     [registerDB.reducerPath]: registerDB.reducer,
     [verifDB.reducerPath]: verifDB.reducer,
     [loginDB.reducerPath]: loginDB.reducer,
+    [recoverDB.reducerPath]: recoverDB.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([registerDB.middleware, verifDB.middleware, loginDB.middleware]),
+    getDefaultMiddleware().concat([
+      registerDB.middleware,
+      verifDB.middleware,
+      loginDB.middleware,
+      recoverDB.middleware,
+    ]),
 });
 
 setupListeners(store.dispatch);
