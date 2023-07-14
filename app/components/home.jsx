82068f7af83a1896024644ad9aa31cc14eb7a939
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-07-13 22:46:40 -0300

    Change Home

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -3,15 +3,15 @@ import Conversations from "./conversations";
 import Interviews from "./interviews";
 
 const Home = () => {
-  return (
-    <section className="w-full p-6 text-pri flex flex-col justify-evenly items-left">
-      <p className="text-xl">¡Hola!</p>
-      <p className="font-medium  text-3xl">Luis Sanchez</p>
-      <Postulations />
-      <Conversations />
-      <Interviews />
-    </section>
-  );
+	return (
+		<section className="w-full p-4 text-pri flex flex-col justify-evenly items-left">
+			<p className="text-xl">¡Hola!</p>
+			<p className="font-medium  text-3xl">Luis Sanchez</p>
+			<Postulations />
+			<Conversations />
+			<Interviews />
+		</section>
+	);
 };
 
 export default Home;
