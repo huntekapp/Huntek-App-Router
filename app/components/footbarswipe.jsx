7a6cf3d94859eca6f051f1e0bf266df8b5e51c23
Matias Gigena <tute910@gmail.com>
2023-07-13 23:24:18 -0300

    cambio

--- app/components/footbarswipe.jsx
+++ app/components/footbarswipe.jsx
@@ -24,14 +24,14 @@ const FootbarSwipe = ({hasSwipedBack, canSwipe, swipe, goBack, showInfo}) => {
             <ReplayIcon style={{fontSize: "2rem", color: "white"}} />
           </IconButton>
         </div>
-        <div className="h-14 w-14 bg-orange-500 border-2 rounded-full shadow-[0px_2px_3px_0px_#000] grid place-content-center">
-          <IconButton onClick={() => showInfo()}>
-            <InfoOutlinedIcon style={{fontSize: "2rem", color: "white"}} />
-          </IconButton>
-        </div>
-        <div className="h-14 w-14 bg-amber-500 border-2 rounded-full shadow-[0px_2px_3px_0px_#000] grid place-content-center">
+        <div className="h-14 w-14 bg-gradient-to-r from-amber-500 to-amber-200 border-2 rounded-full shadow-[0px_2px_3px_0px_#000] grid place-content-center">
           <IconButton onClick={() => swipe("down")}>
             <WorkOutlineIcon style={{fontSize: "2rem", color: "white"}} />
+          </IconButton>
+        </div>
+        <div className="h-14 w-14 bg-green-500 border-2 rounded-full shadow-[0px_2px_3px_0px_#000] grid place-content-center">
+          <IconButton onClick={() => showInfo()}>
+            <InfoOutlinedIcon style={{fontSize: "2.5rem", color: "white"}} />
           </IconButton>
         </div>
         <div
