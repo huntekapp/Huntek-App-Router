abef648b0d4556345e92a5657f14e1ac9c528bf3
Matias Gigena <tute910@gmail.com>
2023-07-17 18:56:48 -0300

    fix

--- app/components/interviews.jsx
+++ app/components/interviews.jsx
@@ -11,9 +11,9 @@ const Interviews = () => {
           Ver todas
         </Link>
       </div>
-      <div className="w-full h-[70%] px-4  text-xs xs:text-sm text-pri border rounded-xl shadow-md flex  justify-start items-center">
+      <div className="w-full h-[80%] px-4 py-1  text-xs xs:text-sm text-pri border rounded-xl shadow-md flex  justify-start items-center">
         <span className="w-14 h-[3.5rem] flex-shrink-0 font-bold text-sm border-2 border-pri rounded-full center">18 SEP</span>
-        <p className="text-md ml-6 sm:text-lg btn-ghost hover:bg-pri/70 bg-pri btn flex-shrink text-sec font-semibold ">
+        <p className="text-[0.800rem] ml-6 sm:text-lg btn-ghost hover:bg-pri/70 bg-pri btn flex-shrink text-sec font-semibold ">
           15:30HS &nbsp;|<span className="font-medium text-sec">Coca-Cola</span>
           <LocationOnIcon style={{fontSize: "0.9rem"}} />
         </p>
