ee940f8eeb1b6bc9b45ff0b549c9e38c13cb9dda
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-07-19 10:26:35 -0300

    Middleware

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+
+export async function middleware(request) {
+  const jwt = request.cookies.get("token");
+
+  if (jwt?.value === undefined) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  try {
+    const { payload } = await jwtVerify(jwt?.value, new TextEncoder().encode("dasdasd"));
+    return NextResponse.next();
+  } catch (error) {
+    return NextResponse.redirect(new URL("login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/home", "/swipe", "/profileExtend", "/conversations", "/interviews", "/notifications"],
+};
