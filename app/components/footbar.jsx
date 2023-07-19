274d1fe7920c018300b6c95975ed3dee85942ad1
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-07-19 14:43:42 -0300

    Update Home

--- app/components/footbar.jsx
+++ app/components/footbar.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const Footbar = () => {
 	return (
-		<footer className="w-full h-12 bg-pri shadow-[0px_-8px_12px_0px_#0004] flex flex-row justify-center fixed bottom-0">
-			<article className="absolute bottom-4">
+		<footer className="w-full h-10 bg-pri shadow-[0px_-8px_12px_0px_#0004] flex flex-row justify-center fixed bottom-0">
+			<article className="absolute bottom-2">
 				<Link href="/swipe">
 					<button className="w-16 h-16 bg-sec rounded-full border-2 border-pri grid place-content-center hover:scale-105 duration-150">
 						<Image loading={"eager"} alt="G" src="/huntek/logo/G.svg" width={35} height={35} />
