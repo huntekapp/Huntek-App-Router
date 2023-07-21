b7e9ed814f03982f1a232f3ed43d5e58f575aa1d
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-07-21 11:54:25 -0300

    Add select signup

---
+++ app/select/page.jsx
@@ -0,0 +1,11 @@
+import Selection from "../components/selection";
+
+const Select = () => {
+	return (
+		<section className="w-full h-screen darkrightgrad flex flex-col justify-between items-center">
+			<Selection />
+		</section>
+	);
+};
+
+export default Select;
