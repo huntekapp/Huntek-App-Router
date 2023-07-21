ac89cb3639cac8b377b43defaebd6843ccbd79d1
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-07-21 11:55:31 -0300

    Update signup

--- app/verifymail/page.jsx
+++ app/verifymail/page.jsx
@@ -2,7 +2,7 @@ import VerifyMail from "../components/verifymail";
 
 const Verifypage = () => {
 	return (
-		<section className="min-h-screen gradient flex flex-col justify-evenly items-center">
+		<section className="min-h-screen darkrightgrad flex flex-col justify-evenly items-center">
 			<VerifyMail />
 		</section>
 	);
