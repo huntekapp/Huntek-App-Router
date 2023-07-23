a94d66a6db6242491cfdb2bf40dc639208ea8dd1
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-07-23 19:21:31 -0300

    Update interviews

--- app/components/interviews.jsx
+++ app/components/interviews.jsx
@@ -12,12 +12,12 @@ const Interviews = () => {
 				</Link>
 			</div>
 			<div className="w-full h-20 px-4 bg-sec text-xs xs:text-sm text-pri border rounded-xl shadow-md flex flex-row justify-between items-center">
-				<div className="w-[10%] min-w-[70px]">
+				<div className="w-[1%] min-w-[70px]">
 					<span className="w-14 h-14 text-sm font-semibold border-pri border-2 rounded-full grid place-content-center">
 						18 SEP
 					</span>
 				</div>
-				<div className="w-[90%]">
+				<div className="w-[99%]">
 					<span className="btn w-full bg-pri text-sm xs:text-xl text-sec border-none">
 						15:30hs<span>|</span>
 						<span className="">Huntek</span>
