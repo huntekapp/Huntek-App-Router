2f77ea29c3135fe9c25a2dad59100c0b8d536f3e
Matias Gigena <tute910@gmail.com>
2023-07-23 16:27:31 -0300

    change ui

--- app/components/profile.jsx
+++ app/components/profile.jsx
@@ -2,73 +2,79 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Profile = ({ user, closeProfile, setCloseProfile }) => {
-	return (
-		<section className="w-full h-screen text-pri flex flex-col">
-			<article className="w-full h-[10%] px-6 border-b flex flex-row justify-between items-center">
-				<div className="w-6 h-6"></div>
-				<h1 className="w-full text-center text-3xl font-bold cursor-default">Mi Perfil</h1>
-				<button
-					onClick={() => {
-						document.getElementById("profile").click();
-						setCloseProfile(!closeProfile);
-					}}
-					className="w-6 h-6 rounded-full flex flex-row justify-center items-center relative hover:bg-pri-100 hover:scale-125 duration-150">
-					<Image src="/utils/x_huntek.svg" alt="close" fill={true} className="absolute" />
-				</button>
-			</article>
-			<article className="w-full h-[60%] p-8 flex flex-col justify-evenly items-center">
-				<div className="w-full h-1/4 flex flex-col justify-around items-center">
-					<div className="w-16 h-16 relative">
-						<Image
-							src={user.image}
-							alt="avatar"
-							fill={true}
-							loading={"eager"}
-							className="object-cover rounded-full absolute"
-						/>
-					</div>
-					<div className="flex flex-row items-center">
-						<h3 className="text-2xl font-medium cursor-default">{user.name}</h3>
-						<Link href="/profileExtend" className="w-4 h-4 ml-2 relative">
-							<Image
-								src="/utils/pencil_huntek.svg"
-								alt="pencil"
-								fill={true}
-								className="absolute opacity-70 hover:opacity-100 hover:scale-125 duration-150"
-							/>
-						</Link>
-					</div>
-					<p className="text-xs font-normal cursor-default">{user.email}</p>
-				</div>
-				<div className="w-full h-3/4 text-base flex flex-col justify-around items-center">
-					<Link href="/postulations" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
-						Postulaciones
-						<div className="w-4 h-4 relative">
-							<Image src="/utils/goto_huntek.svg" alt="arrow right" fill={true} className="absolute" />
-						</div>
-					</Link>
-					<Link href="/conversations" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
-						Conversaciones
-						<div className="w-4 h-4 relative">
-							<Image src="/utils/goto_huntek.svg" alt="arrow right" fill={true} className="absolute" />
-						</div>
-					</Link>
-					<Link href="/home" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
-						Términos & Condiciones
-						<div className="w-4 h-4 relative">
-							<Image src="/utils/goto_huntek.svg" alt="arrow right" fill={true} className="absolute" />
-						</div>
-					</Link>
-				</div>
-			</article>
-			<article className="w-full h-[30%] p-8 text-pri-500 flex flex-col justify-evenly items-center">
-				<Link href={"/login"} className="text-red-700 hover:opacity-70">
-					Cerrar sesión
-				</Link>
-				Copyright © 2023 Huntek App
-			</article>
-		</section>
-	);
+  return (
+    <section className="w-full h-screen text-pri flex flex-col">
+      <article className="w-full h-[10%] px-6 border-b flex flex-row justify-between items-center">
+        <div className="w-6 h-6"></div>
+        <h1 className="w-full text-center text-3xl font-bold cursor-default">Mi Perfil</h1>
+        <button
+          onClick={() => {
+            document.getElementById("profile").click();
+            setCloseProfile(!closeProfile);
+          }}
+          className="w-6 h-6 rounded-full flex flex-row justify-center items-center relative hover:bg-pri-100 hover:scale-125 duration-150">
+          <Image src="/utils/x_huntek.svg" alt="close" fill={true} className="absolute" />
+        </button>
+      </article>
+      <article className="w-full h-full p-8 flex flex-col justify-evenly items-center">
+        <div className="w-full h-2/4 flex flex-col justify-around items-center">
+          <div className="w-20 h-20 relative">
+            <Image
+              src={user.image}
+              alt="avatar"
+              fill={true}
+              loading={"eager"}
+              className="object-cover rounded-full absolute"
+            />
+          </div>
+          <div className="flex flex-row items-center">
+            <h3 className="text-2xl font-medium cursor-default">{user.name}</h3>
+            <Link href="/profileExtend" className="w-4 h-4 ml-2 relative">
+              <Image
+                src="/utils/pencil_huntek.svg"
+                alt="pencil"
+                fill={true}
+                className="absolute opacity-70 hover:opacity-100 hover:scale-125 duration-150"
+              />
+            </Link>
+          </div>
+          <p className="text-xs font-normal cursor-default">{user.email}</p>
+        </div>
+        <div className="w-full h-full mt-5  text-base flex flex-col justify-around items-center">
+          <Link href="/postulations" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
+            Postulaciones
+            <div className="w-4 h-4 relative">
+              <Image src="/utils/goto_huntek.svg" alt="arrow right" fill={true} className="absolute" />
+            </div>
+          </Link>
+          <Link href="/conversations" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
+            Conversaciones
+            <div className="w-4 h-4 relative">
+              <Image src="/utils/goto_huntek.svg" alt="arrow right" fill={true} className="absolute" />
+            </div>
+          </Link>
+          <Link href="/home" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
+            Configuración
+            <div className="w-4 h-4 relative">
+              <Image src="/utils/goto_huntek.svg" alt="arrow right" fill={true} className="absolute" />
+            </div>
+          </Link>
+          <Link href="/home" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
+            Términos & Condiciones
+            <div className="w-4 h-4 relative">
+              <Image src="/utils/goto_huntek.svg" alt="arrow right" fill={true} className="absolute" />
+            </div>
+          </Link>
+        </div>
+      </article>
+      <article className="w-full h-[20%] mb-3  px-8  text-pri-500 flex flex-col justify-evenly items-center">
+        <Link href={"/login"} className="text-red-700 hover:opacity-70">
+          Cerrar sesión
+        </Link>
+        Copyright © 2023 Huntek App
+      </article>
+    </section>
+  );
 };
 
 export default Profile;
