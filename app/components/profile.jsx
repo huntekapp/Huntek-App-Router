0bc5f674549ce8d9e4cc570d6730aa3656ced0a7
Matias Gigena <tute910@gmail.com>
2023-07-26 13:48:03 -0300

    add better navigation + cerrar sesion.

--- app/components/profile.jsx
+++ app/components/profile.jsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
@@ -5,66 +6,73 @@ import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import NavigateNextOutlinedIcon from "@mui/icons-material/NavigateNextOutlined";
 
 const Profile = ({ user, closeProfile, setCloseProfile }) => {
-	return (
-		<section className="w-full h-screen text-pri flex flex-col">
-			<article className="w-full h-[10%] px-4 border-b flex flex-row justify-between items-center">
-				<div className="w-6 h-6"></div>
-				<h1 className="w-full text-center text-3xl font-bold cursor-default">Mi Perfil</h1>
-				<button
-					onClick={() => {
-						document.getElementById("profile").click();
-						setCloseProfile(!closeProfile);
-					}}
-					className="w-6 h-6 rounded-full grid place-content-center hover:bg-pri-100 hover:scale-125 duration-150">
-					<CloseOutlinedIcon />
-				</button>
-			</article>
-			<article className="w-full h-[90%] p-8 flex flex-col justify-evenly items-center">
-				<div className="w-full h-2/5 flex flex-col justify-center items-center">
-					<div className="w-20 h-20 mb-2 relative">
-						<Image
-							src={user.image}
-							alt="avatar"
-							fill={true}
-							loading={"eager"}
-							className="object-cover rounded-full absolute"
-						/>
-					</div>
-					<div className="flex flex-row items-center">
-						<h3 className="text-2xl font-medium cursor-default">{user.name}</h3>
-						<Link href="/profileExtend" className="ml-2">
-							<EditOutlinedIcon className="w-5 h-5 opacity-70 hover:opacity-100 hover:scale-125 duration-150" />
-						</Link>
-					</div>
-					<p className="text-xs font-normal cursor-default">{user.email}</p>
-				</div>
-				<div className="w-full h-4/5 text-base flex flex-col justify-around items-center">
-					<Link href="/postulations" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
-						Postulaciones
-						<NavigateNextOutlinedIcon />
-					</Link>
-					<Link href="/conversations" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
-						Conversaciones
-						<NavigateNextOutlinedIcon />
-					</Link>
-					<Link href="/home" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
-						Configuración
-						<NavigateNextOutlinedIcon />
-					</Link>
-					<Link href="/home" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
-						Términos & Condiciones
-						<NavigateNextOutlinedIcon />
-					</Link>
-				</div>
-				<div className="w-full h-1/4 p-8 text-pri-500 flex flex-col justify-evenly items-center">
-					<Link href={"/login"} className="text-red-700 hover:opacity-70">
-						Cerrar sesión
-					</Link>
-					Copyright © 2023 Huntek App
-				</div>
-			</article>
-		</section>
-	);
+  const router = useRouter();
+  return (
+    <section className="w-full h-screen text-pri flex flex-col">
+      <article className="w-full h-[10%] px-4 border-b flex flex-row justify-between items-center">
+        <div className="w-6 h-6"></div>
+        <h1 className="w-full text-center text-3xl font-bold cursor-default">Mi Perfil</h1>
+        <button
+          onClick={() => {
+            document.getElementById("profile").click();
+            setCloseProfile(!closeProfile);
+          }}
+          className="w-6 h-6 rounded-full grid place-content-center hover:bg-pri-100 hover:scale-125 duration-150">
+          <CloseOutlinedIcon />
+        </button>
+      </article>
+      <article className="w-full h-[90%] p-8 flex flex-col justify-evenly items-center">
+        <div className="w-full h-2/5 flex flex-col justify-center items-center">
+          <div className="w-20 h-20 mb-2 relative">
+            <Image
+              src={user.image}
+              alt="avatar"
+              fill={true}
+              loading={"eager"}
+              className="object-cover rounded-full absolute"
+            />
+          </div>
+          <div className="flex flex-row items-center">
+            <h3 className="text-2xl font-medium cursor-default">{user.name}</h3>
+            <Link href="/profileExtend" className="ml-2">
+              <EditOutlinedIcon className="w-5 h-5 opacity-70 hover:opacity-100 hover:scale-125 duration-150" />
+            </Link>
+          </div>
+          <p className="text-xs font-normal cursor-default">{user.email}</p>
+        </div>
+        <div className="w-full h-4/5 text-base flex flex-col justify-around items-center">
+          <Link href="/postulations" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
+            Postulaciones
+            <NavigateNextOutlinedIcon />
+          </Link>
+          <Link href="/conversations" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
+            Conversaciones
+            <NavigateNextOutlinedIcon />
+          </Link>
+          <Link href="/home" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
+            Configuración
+            <NavigateNextOutlinedIcon />
+          </Link>
+          <Link href="/home" className="w-full h-1/4 flex justify-between items-center hover:opacity-70">
+            Términos & Condiciones
+            <NavigateNextOutlinedIcon />
+          </Link>
+        </div>
+        <div className="w-full h-1/4 p-8 text-pri-500 flex flex-col justify-evenly items-center">
+          <button
+            className="text-red-700 hover:opacity-70"
+            onClick={() => {
+              document.cookie = "kTnKETkt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+              document.cookie = "AiOiJKV1Q=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+              router.push("/login");
+            }}>
+            Cerrar sesión
+          </button>
+          Copyright © 2023 Huntek App
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default Profile;
--- middleware.js
+++ middleware.js
@@ -3,24 +3,48 @@ import { jwtVerify } from "jose";
 
 export async function middleware(request) {
   const jwt = request.cookies.get("kTnKETkt");
-  const response = NextResponse.next()
-  if (jwt?.value === undefined) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  const response = NextResponse.next();
+
+  const protectedPaths = ["/home", "/swipe", "/profileExtend", "/conversations", "/interviews", "/notifications"];
+  if (protectedPaths.some((path) => request.nextUrl.pathname.startsWith(path))) {
+    if (jwt?.value === undefined) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 7);
+    try {
+      const { payload } = await jwtVerify(
+        jwt?.value,
+        new TextEncoder().encode("DSxSz^r#r@6u^ZFkipmgySATzypg@&MMyuW@Kigp")
+      );
+      const AiOiJKV1Q = payload.user_info.id;
+      response.cookies.set({
+        name: "AiOiJKV1Q",
+        value: AiOiJKV1Q,
+        expires: expirationDate,
+      });
+      return response;
+    } catch (error) {
+      return NextResponse.redirect(new URL("login", request.url));
+    }
   }
 
-  try {
-    const { payload } = await jwtVerify(jwt?.value, new TextEncoder().encode("DSxSz^r#r@6u^ZFkipmgySATzypg@&MMyuW@Kigp"));
-    const AiOiJKV1Q = payload.user_info.id
-    response.cookies.set({
-      name: 'AiOiJKV1Q',
-      value: AiOiJKV1Q,
-    });
-    return response
-  } catch (error) {
-    return NextResponse.redirect(new URL("login", request.url));
+  const publicPaths = ["/login", "/signup", "/recoverpassword"];
+  if (publicPaths.some((path) => request.nextUrl.pathname.startsWith(path))) {
+    if (jwt?.value !== undefined) {
+      try {
+        const { payload } = await jwtVerify(
+          jwt?.value,
+          new TextEncoder().encode("DSxSz^r#r@6u^ZFkipmgySATzypg@&MMyuW@Kigp")
+        );
+        if (payload.user_info) {
+          return NextResponse.redirect(new URL("/home", request.url));
+        }
+      } catch (error) {
+        // Nada.
+      }
+    }
   }
+
+  return response;
 }
-
-export const config = {
-  matcher: ["/home", "/swipe", "/profileExtend", "/conversations", "/interviews", "/notifications"],
-};
