c624d5f88c14db6e74568e97232ce1c432e2c3f1
nicolasfont1 <nicolasfont15@gmail.com>
2023-07-28 01:14:21 -0300

    endpoint updated

--- app/globalstore/services/log-reg-val/useRegister.js
+++ app/globalstore/services/log-reg-val/useRegister.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const registerDB = createApi({
   reducerPath: "registerDB",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://dev.api.app.huntek.com/api/v1/users/create",
+    baseUrl: "https://dev.api.app.huntek.com/api/v1/profile/create",
   }),
   endpoints: (builder) => ({
     postUsers: builder.mutation({
