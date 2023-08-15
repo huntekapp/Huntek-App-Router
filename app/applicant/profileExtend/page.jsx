e1e89f59d0fcfcabc8c4a5e06067447a662029f5
nicolasfont1 <nicolasfont15@gmail.com>
2023-08-14 21:49:39 -0300

    changed bg gradient

--- app/applicant/profileExtend/page.jsx
+++ app/applicant/profileExtend/page.jsx
@@ -3,7 +3,7 @@ import ProfileExt from "../../components/applicant/profile/profileExtend";
 
 const ProfileExtPage = () => {
 	return (
-		<main className="w-full h-screen lightcentergrad flex flex-col justify-between items-center">
+		<main className="w-full h-screen bg-sec flex flex-col justify-between items-center">
 			<NavBar />
 			<ProfileExt />
 		</main>
