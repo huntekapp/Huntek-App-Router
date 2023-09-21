b6a02b14e91ff250fd502c9b98c589e61b684c40
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-09-21 15:13:52 -0300

    update animation

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,24 @@
+"use client";
+import { gsap } from "gsap";
+import { useEffect } from "react";
 import LogForm from "../components/login";
 
 const Login = () => {
+	const hi = () => {
+		var tl = gsap.timeline();
+		tl.from("#logo", { x: 300 }, "+=1");
+		tl.from("#form", { x: -200, opacity: 0 });
+	};
+
+	useEffect(() => {
+		hi();
+	}, []);
+
 	return (
 		<main className="h-screen darkrightgrad flex flex-col lg:flex-row justify-evenly items-center">
-			<div className="w-1/2 h-2/5 lg:w-[450px] lg:h-[450px] bg-HKtext bg-contain bg-center bg-no-repeat"></div>
+			<div
+				id="logo"
+				className="w-1/2 h-2/5 lg:w-[450px] lg:h-[450px] bg-HKtext bg-contain bg-center bg-no-repeat"></div>
 			<LogForm />
 		</main>
 	);
