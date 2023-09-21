58b6e4ab64c5731ccc529374b28820a7e3b354b2
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-09-21 15:12:02 -0300

    delete animation

--- app/page.jsx
+++ app/page.jsx
@@ -1,29 +1,16 @@
 "use client";
-import {useRouter} from "next/navigation";
-import {useEffect, useState} from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Landing() {
 	const router = useRouter();
 	const [ping, setPing] = useState(false);
 
 	useEffect(() => {
-		const handleAnimationEnd = () => {
-			router.push("/login");
-		};
-		const animate = setTimeout(() => {
-			setTimeout(() => {
-				setPing(true);
-				handleAnimationEnd();
-			}, 200);
-		}, 5000);
-		return () => {
-			clearTimeout(animate);
-		};
+		router.push("/login");
 	}, []);
 
 	return (
-		<section className="w-screen h-screen darkrightgrad grid place-content-center border-2 border-black">
-			<div className={`w-[250px] h-[250px] bg-HKanimate ${ping && "animate-ping"}`}></div>
-		</section>
+		<section className="w-screen h-screen darkrightgrad grid place-content-center border-2 border-black"></section>
 	);
 }
