35367963022a56fa415c243b6683f45b2fb34894
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-09-22 19:27:04 -0300

    Update login route

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,34 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { gsap } from "gsap";
+import { useEffect } from "react";
+import LogForm from "./components/login";
 
-export default function Landing() {
-	const router = useRouter();
-	const [ping, setPing] = useState(false);
+const row = () => {
+	var tl = gsap.timeline();
+	tl.from("#logo", { x: "+70%" }, "+=1");
+	tl.from("#form", { x: -200, opacity: 0 });
+};
 
+const col = () => {
+	var tl = gsap.timeline();
+	tl.from("#logo", { y: "+60%" }, "+=1");
+	tl.from("#form", { y: -200, opacity: 0 });
+};
+
+const Login = () => {
 	useEffect(() => {
-		router.push("/login");
+		const width = window.innerWidth;
+		width < 1000 ? col() : row();
 	}, []);
 
 	return (
-		<section className="w-screen h-screen darkrightgrad grid place-content-center border-2 border-black"></section>
+		<main className="h-screen darkrightgrad flex flex-col lg:flex-row justify-evenly items-center">
+			<div
+				id="logo"
+				className="w-1/2 h-2/5 lg:w-[450px] lg:h-[450px] bg-HKtext bg-contain bg-center bg-no-repeat"></div>
+			<LogForm />
+		</main>
 	);
-}
+};
+
+export default Login;
